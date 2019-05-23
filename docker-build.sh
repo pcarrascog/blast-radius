822cdb2e5755e87503aa0c0dfb30ecc93302bd87
Paulo Carrasco <paulo.carrasco@globant.com>
2019-05-23 11:10:37 -0400

    version tf

--- docker-build.sh
+++ docker-build.sh
@@ -5,7 +5,7 @@ set -e
 cd $1
 
 # Download terraform zip
-terraform_url=https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_${TF_VERSION}_linux_amd64.zip
+terraform_url=https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
 
 echo "Downloading $terraform_url."
 curl -o terraform.zip $terraform_url
